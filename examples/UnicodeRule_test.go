bb72f928c464998494ba0e9d391249856e9c16f6
Ferdinand Neman <ferdinand.neman@gmail.com>
2021-03-16 01:03:07 +0700

    Added example to prove that function name, variable names are now accepting unicode in their identifier.

---
+++ examples/UnicodeRule_test.go
@@ -0,0 +1,63 @@
+package examples
+
+import (
+	"fmt"
+	"github.com/hyperjumptech/grule-rule-engine/ast"
+	"github.com/hyperjumptech/grule-rule-engine/builder"
+	"github.com/hyperjumptech/grule-rule-engine/engine"
+	"github.com/hyperjumptech/grule-rule-engine/pkg"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+type MyḞact struct {
+	IntAttribute     int64
+	StringǍttribute  string
+	BooleanAttribute bool
+	ḞloatAttribute   float64
+	TimeAttribute    time.Time
+	WhatToSay        string
+}
+
+func (mf *MyḞact) GetẀhatToSay(sentence string) string {
+	return fmt.Sprintf("Let say \"%s\"", sentence)
+}
+
+func TestUnicodeTutorial(t *testing.T) {
+	myFact := &MyḞact{
+		IntAttribute:     123,
+		StringǍttribute:  "Some string vǍlue",
+		BooleanAttribute: true,
+		ḞloatAttribute:   1.234,
+		TimeAttribute:    time.Now(),
+	}
+	dataCtx := ast.NewDataContext()
+	err := dataCtx.Add("MḞ", myFact)
+	assert.NoError(t, err)
+
+	// Prepare knowledgebase library and load it with our rule.
+	knowledgeLibrary := ast.NewKnowledgeLibrary()
+	ruleBuilder := builder.NewRuleBuilder(knowledgeLibrary)
+
+	drls := `
+rule CheckValues "Check the default values" salience 10 {
+    when 
+        MḞ.IntAttribute == 123 && MḞ.StringǍttribute == "Some string vǍlue"
+    then
+        MḞ.WhatToSay = MḞ.GetẀhatToSay("HellǑ Grule");
+		Retract("CheckValues");
+}
+`
+	byteArr := pkg.NewBytesResource([]byte(drls))
+	err = ruleBuilder.BuildRuleFromResource("Tutorial", "0.0.1", byteArr)
+	assert.NoError(t, err)
+
+	knowledgeBase := knowledgeLibrary.NewKnowledgeBaseInstance("Tutorial", "0.0.1")
+
+	engine := engine.NewGruleEngine()
+	err = engine.Execute(dataCtx, knowledgeBase)
+	assert.NoError(t, err)
+	assert.Equal(t, "Let say \"HellǑ Grule\"", myFact.WhatToSay)
+	println(myFact.WhatToSay)
+}
