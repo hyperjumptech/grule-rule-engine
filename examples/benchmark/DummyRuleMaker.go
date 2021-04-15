b802e3067522e8d4bcab7b345e7e46cba2ca2714
Ferdinand Neman <ferdinand.neman@gmail.com>
2021-04-15 20:15:56 +0700

    Added performance test (#187)
    * Added performance test
    
    * Fix linting

---
+++ examples/benchmark/DummyRuleMaker.go
@@ -0,0 +1,116 @@
+package benchmark
+
+import (
+	"bufio"
+	"bytes"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var (
+	words    []string
+	dupCheck map[string]bool
+)
+
+func init() {
+	f, err := os.Open("words.txt")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer f.Close()
+	buf := bufio.NewReader(f)
+	words = make([]string, 0)
+	for true {
+		str, err := buf.ReadString('\n')
+		if err != nil {
+			break
+		}
+		words = append(words, strings.TrimSpace(str))
+	}
+	rand.Seed(time.Now().Unix())
+	dupCheck = make(map[string]bool)
+}
+
+// GetWord get a random english word
+func GetWord(t bool) string {
+	w := words[rand.Intn(len(words))]
+	if t {
+		return strings.Title(w)
+	}
+	return w
+}
+
+// MakeRule make a single dummy rule
+func MakeRule(seq int) string {
+	var rname string
+	for true {
+		rname = GetWord(true) + GetWord(true) + GetWord(true)
+		if _, ok := dupCheck[rname]; !ok {
+			break
+		}
+	}
+	buff := &bytes.Buffer{}
+	buff.WriteString("rule ")
+	buff.WriteString(rname)
+	buff.WriteString(" \"")
+	buff.WriteString(strconv.Itoa(seq))
+	buff.WriteString(" ")
+	buff.WriteString(GetWord(true))
+	buff.WriteString(" ")
+	buff.WriteString(GetWord(true))
+	buff.WriteString("\"")
+	buff.WriteString(" salience ")
+	buff.WriteString(strconv.Itoa(rand.Intn(100) + 10))
+	buff.WriteString(" {\n\t")
+	buff.WriteString("when\n\t\t")
+	buff.WriteString(GetWord(false))
+	buff.WriteString(".")
+	buff.WriteString(GetWord(true))
+	buff.WriteString(" == ")
+	buff.WriteString(GetWord(false))
+	buff.WriteString(".")
+	buff.WriteString(GetWord(true))
+	buff.WriteString("() && \n\t\t")
+	buff.WriteString(GetWord(false))
+	buff.WriteString(".")
+	buff.WriteString(GetWord(true))
+	buff.WriteString(" <= ")
+	buff.WriteString(GetWord(false))
+	buff.WriteString(".")
+	buff.WriteString(GetWord(true))
+	buff.WriteString("()\n\tthen\n\t\t")
+	buff.WriteString(GetWord(false))
+	buff.WriteString(".")
+	buff.WriteString(GetWord(true))
+	buff.WriteString("();\n\t\t")
+	buff.WriteString(GetWord(false))
+	buff.WriteString(".")
+	buff.WriteString(GetWord(true))
+	buff.WriteString(" = ")
+	buff.WriteString(GetWord(false))
+	buff.WriteString(".")
+	buff.WriteString(GetWord(true))
+	buff.WriteString("();")
+	buff.WriteString("\n}\n\n")
+
+	return buff.String()
+}
+
+// GenRandomRule simply generate count number of simple parse-able rule into a file
+func GenRandomRule(fileName string, count int) error {
+	f, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY, 666)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	for i := 1; i <= count; i++ {
+		_, err := f.WriteString(MakeRule(i))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ examples/benchmark/GRBPerformance_test.go
@@ -0,0 +1,87 @@
+package benchmark
+
+import (
+	"github.com/hyperjumptech/grule-rule-engine/ast"
+	"github.com/hyperjumptech/grule-rule-engine/builder"
+	"github.com/hyperjumptech/grule-rule-engine/pkg"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+	"time"
+)
+
+var (
+	ruleCount = 5000
+
+	grlFile = "5000_dummy_rules.grl"
+	grbFile = "5000_dummy_rules.grb"
+
+	knowledgeName    = "Dummy Knowledge"
+	knowledgeVersion = "v1.0.0"
+)
+
+func TestSerializationPerformanceOnFile(t *testing.T) {
+	if testing.Short() {
+		return
+	}
+
+	t.Log("GENERATING DUMMY RULES")
+	err := GenRandomRule(grlFile, ruleCount)
+	assert.Nil(t, err)
+
+	t.Log("LOADING GRL")
+	timer := time.Now()
+
+	// SAFING INTO FILE
+	// First prepare our library for loading the orignal GRL
+	lib := ast.NewKnowledgeLibrary()
+	rb := builder.NewRuleBuilder(lib)
+	err = rb.BuildRuleFromResource(knowledgeName, knowledgeVersion, pkg.NewFileResource(grlFile))
+	assert.NoError(t, err)
+
+	durationA := time.Since(timer)
+	t.Log("SAVING BINARY INTO GRB")
+	timer = time.Now()
+
+	// Lets create the file to write into
+	f, err := os.OpenFile(grbFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	assert.Nil(t, err)
+
+	// Save the knowledge base into the file and close it.
+	err = lib.StoreKnowledgeBaseToWriter(f, knowledgeName, knowledgeVersion)
+	assert.Nil(t, err)
+	_ = f.Close()
+
+	durationB := time.Since(timer)
+	t.Log("LOADING BINARY FROM GRB")
+	timer = time.Now()
+
+	// LOADING FROM FILE
+	// Lets assume we are using different library, so create a new one
+	lib2 := ast.NewKnowledgeLibrary()
+
+	// Open the existing safe file
+	f2, err := os.Open(grbFile)
+	assert.Nil(t, err)
+
+	// Load the file directly into the library and close the file
+	kb2, err := lib2.LoadKnowledgeBaseFromReader(f2, true)
+	assert.Nil(t, err)
+	_ = f2.Close()
+
+	durationC := time.Since(timer)
+
+	t.Log("-------------------------------------------")
+	t.Logf("Load GRL duration   : %d ms", durationA.Milliseconds())
+	t.Logf("Saving GRB duration : %d ms", durationB.Milliseconds())
+	t.Logf("Load GRB duration   : %d ms", durationC.Milliseconds())
+
+	// Delete the test file
+	err = os.Remove(grlFile)
+	assert.Nil(t, err)
+	err = os.Remove(grbFile)
+	assert.Nil(t, err)
+
+	// compare that the original knowledgebase is exacly the same to the loaded one.
+	assert.True(t, lib.GetKnowledgeBase(knowledgeName, knowledgeVersion).IsIdentical(kb2))
+}
