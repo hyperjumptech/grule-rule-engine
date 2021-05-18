453c1cd88190417afd07eb542ade5e04a1d670b4
Ferdinand Neman <ferdinand.neman@gmail.com>
2021-05-18 19:06:45 +0700

    Updated README.md to removed featured product and added comment in the GrlParseErrorDetection_test.go so reader can easily understand how to use ErrorReporter

--- examples/GrlParseErrorDetection_test.go
+++ examples/GrlParseErrorDetection_test.go
@@ -20,17 +20,29 @@ rule ErrorRule1 "Rule with error"  salience 10{
 `
 )
 
+// TestParsingErrorDetection demonstrate how to make your own GRL script/syntax validator using the RuleBuilder.
 func TestParsingErrorDetection(t *testing.T) {
+
+	// Normal stuff, creating a library and builder for it.
 	lib := ast.NewKnowledgeLibrary()
 	ruleBuilder := builder.NewRuleBuilder(lib)
+
+	// Build normally
 	err := ruleBuilder.BuildRuleFromResource("Test", "0.1.1", pkg.NewBytesResource([]byte(RuleWithError)))
 
-	if reporter, ok := err.(*pkg.GruleErrorReporter); ok && reporter.HasError() {
-		for i, er := range reporter.Errors {
-			t.Logf("detected error #%d : %s", i, er.Error())
+	// If the err != nil something is wrong.
+	if err != nil {
+		// Cast the error into pkg.GruleErrorReporter with type checking.
+		// Type checking is necessary because the err might not only parsing error.
+		if reporter, ok := err.(*pkg.GruleErrorReporter); ok {
+			// Lets iterate all the error we get during parsing.
+			for i, er := range reporter.Errors {
+				t.Logf("detected error #%d : %s", i, er.Error())
+			}
+		} else {
+			// Well, its an error but not GruleErrorReporter instance. could be IO error.
+			t.Error("There should be GruleErrorReporter")
+			t.FailNow()
 		}
-	} else {
-		t.Error("There should be an error")
-		t.FailNow()
 	}
 }
