1801b908480242e62c02a4e6ba51a9516b66970d
Ferdinand Neman <ferdinand.neman@gmail.com>
2022-12-15 12:22:08 +0700

    In the middle of adding playground

--- editor/statics/js/index.js
+++ editor/statics/js/index.js
@@ -39,5 +39,73 @@ function executeRule() {
     }, "json") .fail(function(data) {
         $("#response").val( "Status " + data.status + " : " + data.statusText + ". ResponseText : " + data.responseText);
     });
-
 }
+
+function MarkUp(src) {
+    let re =  /rule/gi;
+    let poses = src.match(re);
+    let marks = []
+    if (poses) {
+        while ((match = re.exec(src)) != null) {
+            marks.push(
+                {
+                    start: match.index,
+                    end: match.index+4,
+                    text: src.substring(match.index,match.index+4),
+                    kind: "rule"
+                }
+            );
+        }
+    }
+    return marks;
+}
+
+function MarkDown(src) {
+    return src;
+}
+
+function Mark() {
+    let markers = MarkUp($("#grleditor").text());
+    // do nothing
+}
+
+function JsonEditorTab(e) {
+    if (e.keyCode === 9) { // tab key
+        e.preventDefault();  // this will prevent us from tabbing out of the editor
+
+        // now insert four non-breaking spaces for the tab key
+        var editor = document.getElementById("jsoneditor");
+        var doc = editor.ownerDocument.defaultView;
+        var sel = doc.getSelection();
+        var range = sel.getRangeAt(0);
+
+        var tabNode = document.createTextNode("\u00a0\u00a0\u00a0\u00a0");
+        range.insertNode(tabNode);
+
+        range.setStartAfter(tabNode);
+        range.setEndAfter(tabNode);
+        sel.removeAllRanges();
+        sel.addRange(range);
+    }
+}
+
+function GrlEditorTab(e) {
+    if (e.keyCode === 9) { // tab key
+        e.preventDefault();  // this will prevent us from tabbing out of the editor
+
+        // now insert four non-breaking spaces for the tab key
+        var editor = document.getElementById("grleditor");
+        var doc = editor.ownerDocument.defaultView;
+        var sel = doc.getSelection();
+        var range = sel.getRangeAt(0);
+
+        var tabNode = document.createTextNode("\u00a0\u00a0\u00a0\u00a0");
+        range.insertNode(tabNode);
+
+        range.setStartAfter(tabNode);
+        range.setEndAfter(tabNode);
+        sel.removeAllRanges();
+        sel.addRange(range);
+    }
+}
+
